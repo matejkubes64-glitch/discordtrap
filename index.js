4ee636c13cd5e4d1c5a06e3bff1c19b08c3826fa
matejkubes64-glitch <matejkubes64@gmail.com>
2026-05-27 01:03:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const client = new Client({
 });
 
 // !!! SEM DOPLŇ SVOJE ÚDAJE !!!
-const BOT_TOKEN = 'MTUwODk1NTM5NDYyNDcyMDk5Ng.GyO-fQ.iktQHynoDOJBf6OK7asxbXC4IWXGONIA_N5yCg'; 
+const BOT_TOKEN = process.env.BOT_TOKEN; 
 const BAN_CHANNEL_ID = '1508951807479513199'; 
 
 client.once('ready', () => {
